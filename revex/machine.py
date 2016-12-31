74ebfe8af299203c43733257d6defdbdd76eab55
Lucas Wiman <lucas.wiman@gmail.com>
2016-12-30 17:02:24 -0800

    Add minimization.

--- revex/machine.py
+++ revex/machine.py
@@ -34,6 +34,11 @@ class DFA(MultiDiGraph):
     def add_transition(self, from_state, to_state, char):
         if not (self.has_node(from_state) and self.has_node(to_state)):
             raise ValueError('States must be added prior to transitions.')
+        if self.delta[from_state].get(char) == to_state:
+            # Transition already present.
+            return
+        elif self.delta[from_state].get(char) != None:
+            raise ValueError('Already have a transition.')
         self.delta[from_state][char] = to_state
         return self.add_edge(
             from_state, to_state,
@@ -99,43 +104,91 @@ class RegexDFA(DFA):
             nodes = next_nodes
 
 
-def minimize_dfa(dfa):
+def get_equivalent_states(dfa):
     """
-    Return a minimized DFA, as constructed using the Myhill-Nerode theorem
-    here: https://cse.sc.edu/~fenner/csce551/minimization.pdf
+    Return equivalent states in the DFA, as constructed using the Myhill-Nerode
+    theorem here: https://cse.sc.edu/~fenner/csce551/minimization.pdf
 
     See also http://www8.cs.umu.se/kurser/TDBC92/VT06/final/1.pdf for more background,
     and https://www.tutorialspoint.com/automata_theory/dfa_minimization.htm for diagrams.
 
+    TODO: this algorithm doesn't does twice as much work as needed by not ordering
+    the states.
     """
-    raise NotImplementedError  # TODO: test & work out bugs.
     states = list(dfa.nodes())
     F = {state for state in states if dfa.node[state]['accepting']}
-    marked = set()
-    unmarked = set()
+
+    # Two nodes p and q in the DFA are considered _equivalent_ iff for every
+    # string S=c0c1c2...ck, starting off at p and q are always accepting or not
+    # accepting.
+
+    # Start off assuming that _all_ node pairs are equivalent, then search for
+    # disproofs of equivalency.
+    equivalent = {
+        (p, q) for p in states for q in states
+
+        # First remove pairs whose equivalence is disproved by the empty string
+        # (i.e. one is an accepting state and the other is not).
+        if (p in F) == (q in F)
+    }
 
     def delta(state, char):
         return dfa.delta[state][char]
 
-    for i, p in enumerate(states):
-        for q in states[i+1:]:
-            if (p in F) != (q in F):
-                marked.add((p, q))
-            else:
-                unmarked.add((p, q))
-
-    update = True
-    while update:
-        update = False
-        for p, q in list(unmarked):
+    # Now proceed in a backtracking search for all 1-character disproofs of
+    # equivalency, 2-character disproofs, and so on. When no new disproofs are
+    # found, we are done.
+    found_disproof = True
+    while found_disproof:
+        found_disproof = False
+        to_disprove = set(equivalent)
+        while to_disprove:
+            p, q = to_disprove.pop()
+            to_disprove.discard((q, p))
             for a in dfa.alphabet:
-                if (delta(p, a), delta(q, a)) in marked:
-                    unmarked.remove((p, q))
-                    marked.add((p, q))
-                    update = True
+                if (delta(p, a), delta(q, a)) not in equivalent:
+                    equivalent.remove((p, q))
+                    found_disproof = True
                     break
 
-    def is_equivalent(p, q, marked=frozenset(marked)):
-        return (p, q) in unmarked
+    return equivalent | {(q ,p) for p, q in equivalent}
 
-    return is_equivalent, unmarked
+
+def minimize_dfa(dfa):
+    """
+    Constructs a minimized DFA by combining equivalent states.
+    """
+    equivalent_states = get_equivalent_states(dfa)
+    equivalency_classes = []
+    old_states = set(dfa.nodes())
+    while old_states:
+        state = old_states.pop()
+        new_state = {state}
+        for (p, q) in equivalent_states:
+            if p == state:
+                new_state.add(q)
+                old_states.discard(q)
+        equivalency_classes.append(frozenset(new_state))
+    old_state_to_new_state = {
+        state: new_state for new_state in equivalency_classes
+        for state in new_state
+    }
+
+    def is_accepting(new_state):
+        return dfa.node[next(iter(new_state))]['accepting']
+
+    start = old_state_to_new_state[dfa.start]
+    new_dfa = DFA(start, is_accepting(start), alphabet=dfa.alphabet)
+    for state in equivalency_classes:
+        if state != start:
+            new_dfa.add_state(state, is_accepting(state))
+
+    for from_state, trans in dfa.delta.items():
+        for char, to_state in trans.items():
+            new_dfa.add_transition(
+                old_state_to_new_state[from_state],
+                old_state_to_new_state[to_state],
+                char,
+            )
+
+    return new_dfa
--- revex/tests/test_machine.py
+++ revex/tests/test_machine.py
@@ -6,7 +6,7 @@ from hypothesis import given, example
 from hypothesis import strategies as st
 
 import revex
-
+from revex.machine import DFA, get_equivalent_states, minimize_dfa
 
 example_regex = revex.compile(r'a[abc]*b[abc]*c')
 example_dfa = example_regex.as_dfa(alphabet='abcd')
@@ -23,3 +23,61 @@ def test_derivative_matches_builtin(s):
 @example('abbbbc')
 def test_dfa_matches_builtin(s):
     assert example_dfa.matches(s) == bool(example_builtin_regex.match(s))
+
+
+def test_equivalent_state_computation():
+    # Construct a DFA where all states are equivalent to each other.
+    alphabet = '01'
+    dfa = DFA(0, True, alphabet=alphabet)
+    dfa.add_state(1, True)
+    dfa.add_state(2, True)
+    dfa.add_state(3, True)
+    dfa.add_transition(0, 1, '0')
+    dfa.add_transition(0, 0, '1')
+    dfa.add_transition(1, 2, '0')
+    dfa.add_transition(1, 1, '1')
+    dfa.add_transition(2, 3, '0')
+    dfa.add_transition(2, 2, '1')
+    dfa.add_transition(3, 0, '0')
+    dfa.add_transition(3, 3, '1')
+    equivalent = get_equivalent_states(dfa)
+    states = [0, 1, 2, 3]
+    assert equivalent == {(p, q) for p in states for q in states}
+
+def test_equivalent_state_example():
+    # Construct the DFA in this example:
+    # https://www.tutorialspoint.com/automata_theory/dfa_minimization.htm
+    alphabet = '01'
+    a, b, c, d, e, f = states = 'abcdef'
+    dfa = DFA(a, False, alphabet=alphabet)
+    dfa.add_state(b, False)
+    dfa.add_state(c, True)
+    dfa.add_state(d, True)
+    dfa.add_state(e, True)
+    dfa.add_state(f, False)
+
+    dfa.add_transition(a, b, '0')
+    dfa.add_transition(a, c, '1')
+
+    dfa.add_transition(b, a, '0')
+    dfa.add_transition(b, d, '1')
+
+    dfa.add_transition(c, e, '0')
+    dfa.add_transition(c, f, '1')
+
+    dfa.add_transition(d, e, '0')
+    dfa.add_transition(d, f, '1')
+
+    dfa.add_transition(e, e, '0')
+    dfa.add_transition(e, f, '1')
+
+    dfa.add_transition(f, f, '0')
+    dfa.add_transition(f, f, '1')
+    expected = {(a, b), (c, d), (c, e), (d, e)}
+    expected |= {(x, x) for x in states}
+    expected |= {(p, q) for (q, p) in expected}
+
+    equivalent = get_equivalent_states(dfa)
+    assert equivalent == expected
+    new_dfa = minimize_dfa(dfa)
+    assert False, 'Add assertions about minimized DFA.'
