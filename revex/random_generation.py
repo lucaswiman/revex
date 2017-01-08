413e7be86656a728334f5767ce177a9ddaf5a660
Lucas Wiman <lucas.wiman@gmail.com>
2017-01-07 16:13:58 -0800

    Factor out BFS count maintenance.

--- revex/random_generation.py
+++ revex/random_generation.py
@@ -35,6 +35,57 @@ class DiscreteRandomVariable(list):
         return bisect_left(self, random.random())
 
 
+class PathCounts(list):
+
+    def __init__(self, dfa, numerical_type=float):
+        """
+        Class for maintaining state path counts inside a dfa.
+
+        Denoted by l_{p,n} in section 2 of the Bernardi & Giménez paper,
+        path_counts[state][n] is the number of paths of length n from
+        state to _some_ final/accepting state.
+
+        Since these numbers can be exponentially large in `n`, it is more
+        efficient to use floating point numbers, but this can be adjusted by
+        specifying the `numerical_type` argument (float or int).
+
+        `dfa` MUST have consecutive integer states, with 0 as the start state,
+        though this is not validated.
+        """
+        self.dfa = dfa
+        self.numerical_type = numerical_type
+        # Initialize the array with the number of zero-length paths from the
+        # state to an accepting state. (i.e. 1 if the state is accepting.)
+        self.states = range(len(self.dfa.node))
+        super(PathCounts, self).__init__(
+            [numerical_type(1)
+             if self.dfa.node[state]['accepting'] else numerical_type(0)]
+            for state in self.states
+        )
+        self.longest_path_length = 0
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            node, path_length = item
+            while path_length > self.longest_path_length:
+                # Precomputes all path lengths up to path_length by expanding
+                # breadth first search.
+                for state in self.states:
+                    # Compute the next length of paths by summing the number of
+                    # paths of length self.longest_path_length among the out-edges
+                    # of the node.
+                    path_counts = sum(
+                        self[self.dfa.delta[state][char]][self.longest_path_length]
+                        for char in self.dfa.alphabet)
+                    self[state].append(path_counts)
+                self.longest_path_length += 1
+            return self[node][path_length]
+        return list.__getitem__(self, item)
+
+    def __repr__(self):
+        return 'PathCounts(%r, numerical_type=%r)' % (self.dfa, self.numerical_type)
+
+
 class RandomRegularLanguageGenerator(object):
     """
     Based off the "Recursive RGA" algorithm described in Bernardi & Giménez,
@@ -61,29 +112,14 @@ class RandomRegularLanguageGenerator(object):
         # path_counts[state][n] is the number of paths of length n from
         # state to _some_ final/accepting state. Since these numbers can
         # be exponentially large in `n`, we use floating point numbers for efficiency.
-        self.path_counts = [
-            # Initialize the array with the number of zero-length paths from the
-            # state to an accepting state. (i.e. 1 if the state is accepting.)
-            [1.0 if self.dfa.node[state]['accepting'] else 0.0]
-            for state in self.nodes
-        ]
-        self.n = 0
+        self.path_counts = PathCounts(self.dfa, numerical_type=float)
         self.node_length_to_character_dist = {}
-
-    def _precompute_l(self, n):
-        while self.n < n:
-            for state in self.nodes:
-                self.path_counts[state].append(
-                    sum(self.path_counts[self.dfa.delta[state][char]][self.n]
-                        for char in self.dfa.alphabet)
-                )
-            self.n += 1
 
     def get_dist_for_node_and_length(self, node, length):
         if (node, length) not in self.node_length_to_character_dist:
             try:
                 dist = DiscreteRandomVariable([
-                    self.path_counts[self.dfa.delta[node][char]][length - 1]
+                    self.path_counts[self.dfa.delta[node][char], length - 1]
                     for char in self.alphabet
                 ])
             except InvalidDistributionError:
@@ -99,7 +135,6 @@ class RandomRegularLanguageGenerator(object):
 
         Returns `None` if no such string exists.
         """
-        self._precompute_l(length)
         state = self.dfa.start
         chars = []
         if length == 0 and not self.dfa.node[state]['accepting']:
@@ -115,6 +150,5 @@ class RandomRegularLanguageGenerator(object):
 
     def valid_lengths_iter(self):
         for length in count():
-            self._precompute_l(length)
-            if self.path_counts[self.dfa.start][length] > 0:
+            if self.path_counts[self.dfa.start, length] > 0:
                 yield length
