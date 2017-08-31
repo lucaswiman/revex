d3b17f8c1c536c6cf085c5b16d079976d74113c2
Lucas Wiman <lucas.wiman@gmail.com>
2017-08-30 17:29:32 -0700

    Add extra imports.

--- revex/__init__.py
+++ revex/__init__.py
@@ -4,7 +4,7 @@ from .derivative import RegularExpression  # noqa
 from .derivative import RegexVisitor
 from .dfa import DEFAULT_ALPHABET
 from .dfa import DFA, String  # noqa
-
+from .generation import DeterministicRegularLanguageGenerator, RandomRegularLanguageGenerator  # noqa
 
 def compile(regex):  # type: (String) -> RegularExpression
     return RegexVisitor().parse(regex)
